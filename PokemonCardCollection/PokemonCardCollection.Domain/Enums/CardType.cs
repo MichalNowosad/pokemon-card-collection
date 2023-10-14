ccb56ea8d9de27d242cf60ceadc24b3f457235ee
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-14 17:44:55 +0200

    clear using

--- PokemonCardCollection/PokemonCardCollection.Domain/Enums/CardType.cs
+++ PokemonCardCollection/PokemonCardCollection.Domain/Enums/CardType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PokemonCardCollection.Domain.Enums
+﻿namespace PokemonCardCollection.Domain.Enums
 {
     public enum CardType
     {
