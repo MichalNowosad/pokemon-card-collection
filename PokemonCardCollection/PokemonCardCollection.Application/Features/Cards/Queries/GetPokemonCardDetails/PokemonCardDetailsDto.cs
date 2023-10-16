ba3eeef30a2be7754aeba941626d42e63b5e807a
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-16 15:46:57 +0200

    add GetPokemonCardDetails query

---
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/Cards/Queries/GetPokemonCardDetails/PokemonCardDetailsDto.cs
@@ -0,0 +1,19 @@
+﻿using PokemonCardCollection.Domain.Enums;
+
+namespace PokemonCardCollection.Application.Features.Cards.Queries.GetPokemonCardDetails
+{
+    public class PokemonCardDetailsDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Number { get; set; }
+        public int HealthPoints { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public EnergyType EnergyType { get; set; }
+        public CardRarity Rarity { get; set; }
+        public string ExpansionName { get; set; } = string.Empty;
+        public string IllustratorName { get; set; } = string.Empty;
+        public PokemonCardAbilityDto? Ability { get; set; }
+        public IEnumerable<PokemonCardAttackDto>? Attacks { get; set; }
+    }
+}
