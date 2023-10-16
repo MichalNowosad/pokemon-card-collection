2720dd7a8906c5aa4c6bdbfd8f10e5af6dbc9c44
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-16 15:51:37 +0200

    add CreatePokemonCard command

---
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/Cards/Commands/CreatePokemonCard/CreatePokemonCardCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PokemonCardCollection.Application.Interfaces.Persistence;
+using PokemonCardCollection.Domain.Entities;
+using PokemonCardCollection.Domain.Enums;
+
+namespace PokemonCardCollection.Application.Features.Cards.Commands.CreatePokemonCard
+{
+    public class CreatePokemonCardCommandHandler : IRequestHandler<CreatePokemonCardCommand, Guid>
+    {
+        private readonly IPokemonCardRepository _pokemonCardRepository;
+        private readonly ICardAttackRepository _cardAttackRepository;
+
+        public CreatePokemonCardCommandHandler(IPokemonCardRepository pokemonCardRepository,
+            ICardAttackRepository cardAttackRepository)
+        {
+            _pokemonCardRepository = pokemonCardRepository ?? throw new ArgumentNullException(nameof(pokemonCardRepository));
+            _cardAttackRepository = cardAttackRepository ?? throw new ArgumentNullException(nameof(cardAttackRepository));
+        }
+
+        public async Task<Guid> Handle(CreatePokemonCardCommand request, CancellationToken cancellationToken)
+        {
+            var pokemonCardDto = request.PokemonCard;
+
+            var cardAttacks = await _cardAttackRepository.GetByIds(pokemonCardDto.AttackIds).ToListAsync();
+
+            var pokemonCard = new PokemonCard
+            {
+                Name = pokemonCardDto.Name,
+                Number = pokemonCardDto.Number,
+                Rarity = pokemonCardDto.Rarity,
+                CardType = CardType.Pokemon,
+                ExpansionId = pokemonCardDto.ExpansionId,
+                IllustratorId = pokemonCardDto.IllustratorId,
+                HealthPoints = pokemonCardDto.HealthPoints,
+                PokemonDescription = pokemonCardDto.PokemonDescription,
+                EnergyType = pokemonCardDto.EnergyType,
+                AbilityId = pokemonCardDto.AbilityId,
+                Attacks = cardAttacks
+            };
+
+            return await _pokemonCardRepository.CreateAsync(pokemonCard);
+        }
+    }
+}
---
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/Cards/Commands/CreatePokemonCard/CreatePokemonCardDto.cs
@@ -0,0 +1,18 @@
+﻿using PokemonCardCollection.Domain.Enums;
+
+namespace PokemonCardCollection.Application.Features.Cards.Commands.CreatePokemonCard
+{
+    public class CreatePokemonCardDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Number { get; set; }
+        public CardRarity Rarity { get; set; }
+        public Guid ExpansionId { get; set; }
+        public Guid IllustratorId { get; set; }
+        public int HealthPoints { get; set; }
+        public string PokemonDescription { get; set; } = string.Empty;
+        public EnergyType EnergyType { get; set; }
+        public Guid AbilityId { get; set; }
+        public IEnumerable<Guid>? AttackIds { get; set; }
+    }
+}
