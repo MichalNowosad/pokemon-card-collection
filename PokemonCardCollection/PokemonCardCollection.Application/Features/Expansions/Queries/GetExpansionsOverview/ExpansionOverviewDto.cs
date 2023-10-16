8ec354f6266d6e5ecafb22115e2c6969bf96735c
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-16 17:21:18 +0200

    get expansions overview query

---
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/Expansions/Queries/GetExpansionsOverview/ExpansionOverviewDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonCardCollection.Application.Features.Expansions.Queries.GetExpansionsOverview
+{
+    public class ExpansionOverviewDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime ReleaseDate { get; set; }
+    }
+}
