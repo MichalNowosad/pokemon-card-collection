5b80408646de18f735af5912ef912a6591df6a76
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-16 17:49:00 +0200

    add get card attack query

---
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/CardAttacks/Queries/GetCardAttackDetails/CardAttackDetailsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonCardCollection.Application.Features.CardAttacks.Queries.GetCardAttackDetails
+{
+    public class CardAttackDetailsDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int? Power { get; set; }
+    }
+}
---
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/CardAttacks/Queries/GetCardAttackDetails/GetCardAttackDetailsCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace PokemonCardCollection.Application.Features.CardAttacks.Queries.GetCardAttackDetails
+{
+    public class GetCardAttackDetailsCommand : IRequest<CardAttackDetailsDto>
+    {
+        public Guid CardAttackId { get; set; }
+    }
+}
