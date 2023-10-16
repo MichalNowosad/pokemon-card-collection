0f3bf56120883641bb1873c252a56047c652738c
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-16 17:41:25 +0200

    add create card attack command

---
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/CardAttacks/Commands/CreateCardAttack/CreateCardAttackDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonCardCollection.Application.Features.CardAttacks.Commands.CreateCardAttack
+{
+    public class CreateCardAttackDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int? Power { get; set; }
+    }
+}
