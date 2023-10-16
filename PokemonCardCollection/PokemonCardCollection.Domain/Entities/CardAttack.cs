508393637b6ac9c5e2905d7b71bbd093976b415d
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-16 17:40:50 +0200

    update card attack model

--- PokemonCardCollection/PokemonCardCollection.Domain/Entities/CardAttack.cs
+++ PokemonCardCollection/PokemonCardCollection.Domain/Entities/CardAttack.cs
@@ -4,8 +4,14 @@ namespace PokemonCardCollection.Domain.Entities
 {
     public class CardAttack : BaseEntity
     {
+        public CardAttack()
+        {
+            PokemonCards = new HashSet<PokemonCard>();
+        }
+
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public int? Power { get; set; }
+        public ICollection<PokemonCard> PokemonCards { get; set; }
     }
 }
