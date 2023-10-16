c8544055234e99ec9879150aaf66a688993f96f3
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-16 17:21:46 +0200

    create expansion command

---
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/Expansions/Commands/CreateExpansion/CreateExpansionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonCardCollection.Application.Features.Expansions.Commands.CreateExpansion
+{
+    public class CreateExpansionDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public int CardsAmount { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Abbreviation { get; set; } = string.Empty;
+    }
+}
