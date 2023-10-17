7084f6a23cb9f70528fd7ce24bfd75715de4d745
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-17 17:29:52 +0200

    add get card abilities overview query

---
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/CardAbilities/Queries/GetCardAbilitiesOverview/CardAbilityOverviewDto.cs
@@ -0,0 +1,7 @@
+﻿namespace PokemonCardCollection.Application.Features.CardAbilities.Queries.GetCardAbilitiesOverview
+{
+    public class CardAbilityOverviewDto
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/CardAbilities/Queries/GetCardAbilitiesOverview/GetCardAbilitiesOverviewQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PokemonCardCollection.Application.Interfaces.Persistence;
+
+namespace PokemonCardCollection.Application.Features.CardAbilities.Queries.GetCardAbilitiesOverview
+{
+    public class GetCardAbilitiesOverviewQueryHandler : IRequestHandler<GetCardAbilitiesOverviewQuery, IEnumerable<CardAbilityOverviewDto>>
+    {
+        private readonly ICardAbilityRepository _cardAbilityRepository;
+
+        public GetCardAbilitiesOverviewQueryHandler(ICardAbilityRepository cardAbilityRepository)
+        {
+            _cardAbilityRepository = cardAbilityRepository ?? throw new ArgumentNullException(nameof(cardAbilityRepository));
+        }
+
+        public async Task<IEnumerable<CardAbilityOverviewDto>> Handle(GetCardAbilitiesOverviewQuery request, CancellationToken cancellationToken)
+        {
+            var cardAbilities = _cardAbilityRepository.GetAllAsync();
+
+            return await cardAbilities.Select(a => new CardAbilityOverviewDto
+            {
+                Name = a.Name
+            }).ToListAsync(cancellationToken: cancellationToken);
+        }
+    }
+}
