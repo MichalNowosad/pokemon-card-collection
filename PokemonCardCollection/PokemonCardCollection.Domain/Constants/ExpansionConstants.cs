3758587821d5d5508bdaf1b66f31e40c9f5c0fea
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-17 17:40:28 +0200

    add constants

---
+++ PokemonCardCollection/PokemonCardCollection.Domain/Constants/ExpansionConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace PokemonCardCollection.Domain.Constants
+{
+    public static class ExpansionConstants
+    {
+        public const int NameMaxLength = 100;
+        public const int AbbreviationMaxLength = 3;
+    }
+}
--- PokemonCardCollection/PokemonCardCollection.Domain/Entities/CardAbility.cs
+++ PokemonCardCollection/PokemonCardCollection.Domain/Entities/CardAbility.cs
@@ -4,7 +4,13 @@ namespace PokemonCardCollection.Domain.Entities
 {
     public class CardAbility : BaseEntity
     {
+        public CardAbility()
+        {
+            PokemonCards = new HashSet<PokemonCard>();
+        }
+
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public ICollection<PokemonCard> PokemonCards { get; set; }
     }
 }
