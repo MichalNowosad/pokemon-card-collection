744ae5dc3b316019eaddfd3d979bde288810fbe3
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-17 17:30:15 +0200

    add create card ability command

---
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/CardAbilities/Commands/CreateCardAbility/CreateCardAbilityCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace PokemonCardCollection.Application.Features.CardAbilities.Commands.CreateCardAbility
+{
+    public class CreateCardAbilityCommand : IRequest<Guid>
+    {
+        public CreateCardAbilityDto? CardAbility { get; set; }
+    }
+}
---
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/CardAbilities/Commands/CreateCardAbility/CreateCardAbilityCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using PokemonCardCollection.Application.Interfaces.Persistence;
+using PokemonCardCollection.Domain.Entities;
+
+namespace PokemonCardCollection.Application.Features.CardAbilities.Commands.CreateCardAbility
+{
+    public class CreateCardAbilityCommandHandler : IRequestHandler<CreateCardAbilityCommand, Guid>
+    {
+        private readonly ICardAbilityRepository _cardAbilityRepository;
+
+        public CreateCardAbilityCommandHandler(ICardAbilityRepository cardAbilityRepository)
+        {
+            _cardAbilityRepository = cardAbilityRepository ?? throw new ArgumentNullException(nameof(cardAbilityRepository));
+        }
+
+        public async Task<Guid> Handle(CreateCardAbilityCommand request, CancellationToken cancellationToken)
+        {
+            var cardAbilityDto = request.CardAbility;
+
+            var cardAbilityToCreate = new CardAbility
+            {
+                Name = cardAbilityDto.Name,
+                Description = cardAbilityDto.Description
+            };
+
+            await _cardAbilityRepository.CreateAsync(cardAbilityToCreate);
+            await _cardAbilityRepository.SaveChangesAsync();
+
+            return cardAbilityToCreate.Id;
+        }
+    }
+}
