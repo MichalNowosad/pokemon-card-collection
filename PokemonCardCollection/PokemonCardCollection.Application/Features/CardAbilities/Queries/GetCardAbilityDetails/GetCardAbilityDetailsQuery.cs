62c1fd2c0b83be0d9e8b672591fc318fa73b4941
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-17 17:30:03 +0200

    add get card ability details query

---
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/CardAbilities/Queries/GetCardAbilityDetails/GetCardAbilityDetailsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace PokemonCardCollection.Application.Features.CardAbilities.Queries.GetCardAbilityDetails
+{
+    public class GetCardAbilityDetailsQuery : IRequest<CardAbilityDetailsDto>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/CardAbilities/Queries/GetCardAbilityDetails/GetCardAbilityDetailsQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PokemonCardCollection.Application.Interfaces.Persistence;
+using PokemonCardCollection.Domain.Entities;
+
+namespace PokemonCardCollection.Application.Features.CardAbilities.Queries.GetCardAbilityDetails
+{
+    public class GetCardAbilityDetailsQueryHandler : IRequestHandler<GetCardAbilityDetailsQuery, CardAbilityDetailsDto>
+    {
+        private readonly ICardAbilityRepository _cardAbilityRepository;
+
+        public GetCardAbilityDetailsQueryHandler(ICardAbilityRepository cardAbilityRepository)
+        {
+            _cardAbilityRepository = cardAbilityRepository ?? throw new ArgumentNullException(nameof(cardAbilityRepository));
+        }
+
+        public async Task<CardAbilityDetailsDto> Handle(GetCardAbilityDetailsQuery request, CancellationToken cancellationToken)
+        {
+            var cardAbility = await _cardAbilityRepository.GetAllAsync()
+                .Select(a => new CardAbility
+                {
+                    Id = a.Id,
+                    Description = a.Description,
+                    Name = a.Name
+                }).FirstOrDefaultAsync(a => a.Id == request.CardAttackId);
+
+            return cardAbility;
+        }
+    }
+}
