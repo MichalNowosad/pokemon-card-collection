a93c54a73b968158f8580578e73d963b5416bf19
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-18 21:52:04 +0200

    validation constants

---
+++ PokemonCardCollection/PokemonCardCollection.Application/Constants/ValidationConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonCardCollection.Application.Constants
+{
+    public static class ValidationConstants
+    {
+        public const string ValueEmptyErrorMessage = "{PropertyName} is required.";
+        public const string MaxLengthErrorMessage = "{PropertyName} must not exceed {MaxLength} characters.";
+    }
+}
