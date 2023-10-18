29a36c1f692ac01934ca9af154ece9c4fafb5181
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-18 21:54:52 +0200

    update pokemon card validator

--- PokemonCardCollection/PokemonCardCollection.Application/Features/Cards/Commands/UpdatePokemonCard/UpdatePokemonCardCommandHandler.cs
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/Cards/Commands/UpdatePokemonCard/UpdatePokemonCardCommandHandler.cs
@@ -1,15 +1,14 @@
 ﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
+using PokemonCardCollection.Application.Features.CardAbilities.Commands.UpdateCardAbility.Validators;
+using PokemonCardCollection.Application.Features.CardAbilities.Commands.UpdateCardAbility;
 using PokemonCardCollection.Application.Interfaces.Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using PokemonCardCollection.Application.Features.Cards.Commands.UpdatePokemonCard.Validators;
 
 namespace PokemonCardCollection.Application.Features.Cards.Commands.UpdatePokemonCard
 {
-    public class UpdatePokemonCardCommandHandler : IRequestHandler<UpdatePokemonCardCommand>
+    public class UpdatePokemonCardCommandHandler : IRequestHandler<UpdatePokemonCardCommand, UpdatePokemonCardCommandResponse>
     {
         private readonly IPokemonCardRepository _pokemonCardRepository;
         private readonly ICardAttackRepository _cardAttackRepository;
@@ -21,8 +20,17 @@ namespace PokemonCardCollection.Application.Features.Cards.Commands.UpdatePokemo
             _cardAttackRepository = cardAttackRepository ?? throw new ArgumentNullException(nameof(cardAttackRepository));
         }
 
-        public async Task Handle(UpdatePokemonCardCommand request, CancellationToken cancellationToken)
+        public async Task<UpdatePokemonCardCommandResponse> Handle(UpdatePokemonCardCommand request, CancellationToken cancellationToken)
         {
+            var validator = new UpdatePokemonCardCommandValidator();
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+            if (validationResult.Errors.Count > 0)
+            {
+                var validationErrorMessages = validationResult.Errors.Select(e => e.ErrorMessage);
+                return new UpdatePokemonCardCommandResponse(HttpStatusCode.UnprocessableEntity, validationErrorMessages);
+            }
+
             var pokemonCardDto = request.PokemonCard;
 
             var pokemonCardToUpdate = await _pokemonCardRepository.GetAsync(pokemonCardDto.Id);
@@ -38,10 +46,12 @@ namespace PokemonCardCollection.Application.Features.Cards.Commands.UpdatePokemo
                 pokemonCardToUpdate.PokemonDescription = pokemonCardDto.PokemonDescription;
                 pokemonCardToUpdate.EnergyType = pokemonCardDto.EnergyType;
                 pokemonCardToUpdate.AbilityId = pokemonCardDto.AbilityId;
-                pokemonCardToUpdate.Attacks = await _cardAttackRepository.GetByIds(pokemonCardDto.AttackIds).ToListAsync();
+                pokemonCardToUpdate.Attacks = await _cardAttackRepository.GetByIds(pokemonCardDto.AttackIds).ToListAsync(cancellationToken: cancellationToken);
 
                 await _pokemonCardRepository.SaveChangesAsync();
             }
+
+            return new UpdatePokemonCardCommandResponse();
         }
     }
 }
