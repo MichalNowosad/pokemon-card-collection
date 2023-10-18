3fc57c3aef3275e60f8ae2609e7661ecc1df4cd8
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-18 23:03:07 +0200

    update model

--- PokemonCardCollection/PokemonCardCollection.Domain/Entities/Expansion.cs
+++ PokemonCardCollection/PokemonCardCollection.Domain/Entities/Expansion.cs
@@ -4,9 +4,17 @@ namespace PokemonCardCollection.Domain.Entities
 {
     public class Expansion : EntityWithFile
     {
+        public Expansion()
+        {
+            PokemonCards = new HashSet<PokemonCard>();
+            TrainerCards = new HashSet<TrainerCard>();
+        }
+
         public string Name { get; set; } = string.Empty;
         public int CardsAmount { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string Abbreviation { get; set; } = string.Empty;
+        public ICollection<PokemonCard> PokemonCards { get; set; }
+        public ICollection<TrainerCard> TrainerCards { get; set; }
     }
 }
--- PokemonCardCollection/PokemonCardCollection.Domain/Entities/Illustrator.cs
+++ PokemonCardCollection/PokemonCardCollection.Domain/Entities/Illustrator.cs
@@ -4,6 +4,14 @@ namespace PokemonCardCollection.Domain.Entities
 {
     public class Illustrator : BaseEntity
     {
+        public Illustrator()
+        {
+            PokemonCards = new HashSet<PokemonCard>();
+            TrainerCards = new HashSet<TrainerCard>();
+        }
+
         public string Name { get; set; } = string.Empty;
+        public ICollection<PokemonCard> PokemonCards { get; set; }
+        public ICollection<TrainerCard> TrainerCards { get; set; }
     }
 }
