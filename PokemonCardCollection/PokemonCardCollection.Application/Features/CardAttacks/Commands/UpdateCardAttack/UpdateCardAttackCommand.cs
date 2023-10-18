f3c91e20bbe7dc98495b733d98faa2219a7a1f6e
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-18 21:53:29 +0200

    update card attack validator

--- PokemonCardCollection/PokemonCardCollection.Application/Features/CardAttacks/Commands/UpdateCardAttack/UpdateCardAttackCommand.cs
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/CardAttacks/Commands/UpdateCardAttack/UpdateCardAttackCommand.cs
@@ -2,8 +2,8 @@
 
 namespace PokemonCardCollection.Application.Features.CardAttacks.Commands.UpdateCardAttack
 {
-    public class UpdateCardAttackCommand : IRequest
+    public class UpdateCardAttackCommand : IRequest<UpdateCardAttackCommandResponse>
     {
-        public UpdateCardAttackDto? CardAttack { get; set; }
+        public UpdateCardAttackDto CardAttack { get; set; } = new UpdateCardAttackDto();
     }
 }
---
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/CardAttacks/Commands/UpdateCardAttack/Validators/UpdateCardAttackCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace PokemonCardCollection.Application.Features.CardAttacks.Commands.UpdateCardAttack.Validators
+{
+    public class UpdateCardAttackCommandValidator : AbstractValidator<UpdateCardAttackCommand>
+    {
+        public UpdateCardAttackCommandValidator()
+        {
+            RuleFor(x => x.CardAttack)
+                .SetValidator(new UpdateCardAttackDtoValidator());
+        }
+    }
+}
