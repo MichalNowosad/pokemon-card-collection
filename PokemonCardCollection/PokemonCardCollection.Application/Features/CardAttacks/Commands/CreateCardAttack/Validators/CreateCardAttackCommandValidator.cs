ef5bc4112939edae2d407a8e7f65a78bd67b71e9
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-18 21:53:04 +0200

    create card attack validator

---
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/CardAttacks/Commands/CreateCardAttack/Validators/CreateCardAttackCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace PokemonCardCollection.Application.Features.CardAttacks.Commands.CreateCardAttack.Validators
+{
+    public class CreateCardAttackCommandValidator : AbstractValidator<CreateCardAttackCommand>
+    {
+        public CreateCardAttackCommandValidator()
+        {
+            RuleFor(x => x.CardAttack)
+                .SetValidator(new CreateCardAttackDtoValidator());
+        }
+    }
+}
