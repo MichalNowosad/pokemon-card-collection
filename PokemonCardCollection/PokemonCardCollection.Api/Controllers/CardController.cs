c8992a3efcf0c20d8b4c77be5f79a0ce061ff62c
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-19 23:16:12 +0200

    add card controller

---
+++ PokemonCardCollection/PokemonCardCollection.Api/Controllers/CardController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PokemonCardCollection.Application.Features.Cards.Queries.GetCardsOverviewByExpansion;
+
+namespace PokemonCardCollection.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CardController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CardController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpGet("expansion/{expansionId}")]
+        public async Task<IActionResult> GetCardsOverview(Guid expansionId)
+        {
+            var allCards = await _mediator.Send(new GetCardsOverviewByExpansionQuery(expansionId));
+
+            return Ok(allCards);
+        }
+    }
+}
