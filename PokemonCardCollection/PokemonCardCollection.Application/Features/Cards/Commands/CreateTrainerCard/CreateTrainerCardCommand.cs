309086b21e86fbb59f67f5e791240d42873e9e47
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-19 23:15:57 +0200

    add trainer card controller

--- PokemonCardCollection/PokemonCardCollection.Application/Features/Cards/Commands/CreateTrainerCard/CreateTrainerCardCommand.cs
+++ PokemonCardCollection/PokemonCardCollection.Application/Features/Cards/Commands/CreateTrainerCard/CreateTrainerCardCommand.cs
@@ -1,14 +1,15 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace PokemonCardCollection.Application.Features.Cards.Commands.CreateTrainerCard
 {
     public class CreateTrainerCardCommand : IRequest<CreateTrainerCardCommandResponse>
     {
-        public CreateTrainerCardCommand(CreateTrainerCardDto trainerCard, IFormFile trainerCardImage)
+        public CreateTrainerCardCommand(IFormCollection form)
         {
-            TrainerCard = trainerCard;
-            TrainerCardImage = trainerCardImage;
+            TrainerCard = JsonConvert.DeserializeObject<CreateTrainerCardDto>(form["pokemonCard"].ToString()) ?? new CreateTrainerCardDto();
+            TrainerCardImage = form.Files[0];
         }
 
         public CreateTrainerCardDto TrainerCard { get; set; }
