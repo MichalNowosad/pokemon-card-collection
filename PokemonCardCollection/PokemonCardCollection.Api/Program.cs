9d83efd5ab0dad089180bc12de55fed0e0b6ffa5
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2023-10-20 15:22:47 +0200

    Create database automatically

--- PokemonCardCollection/PokemonCardCollection.Api/Program.cs
+++ PokemonCardCollection/PokemonCardCollection.Api/Program.cs
@@ -3,8 +3,7 @@ using PokemonCardCollection.Api;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder
     .ConfigureServices()
-    .ConfigurePipeline();
-
-app.MapGet("/", () => "Hello World!");
+    .ConfigurePipeline()
+    .ConfigureDatabase();
 
 app.Run();
